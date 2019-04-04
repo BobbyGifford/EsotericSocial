a85486e674b2796ea04216dc80a4a764b1453431
Bobby <bobby077joe@gmail.com>
2019-04-04 11:44:13 -0700

    Added keys file

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,13 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const keys = require("./config/keys");
 const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
 const app = express();
 
 mongoose
-  .connect(
-    "mongodb+srv://bobby:bobby@esotericsocial-bhe06.mongodb.net/test?retryWrites=true",
-    { useNewUrlParser: true, useCreateIndex: true }
-  )
+  .connect(keys.mongoURI, { useNewUrlParser: true, useCreateIndex: true })
   .then(() => {
     console.log("Connected to db");
   })
