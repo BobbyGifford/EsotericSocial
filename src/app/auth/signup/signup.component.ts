ab22ce9d08d438ab4ac2ffd8cf8a7a84fba50c9f
Bobby <bobby077joe@gmail.com>
2019-04-04 17:59:14 -0700

    Added loader to login/signup.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -8,12 +8,14 @@ import { AuthService } from "../auth.service";
   styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent {
+  isLoading = false;
   constructor(public authService: AuthService) {}
 
   onSignup(form: NgForm) {
     if (form.invalid) {
       return;
     }
+    this.isLoading = true;
     this.authService.createUser(form.value.email, form.value.password);
   }
 }
